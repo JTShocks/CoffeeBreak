df34bf7123aa91580ffad311208b43a81033d9cd
JTShocks <jacobdreyer2002@gmail.com>
2025-01-17 20:41:45 -0500

    Score tracking values and similar

--- Assets/_Scripts/EventManager.cs
+++ Assets/_Scripts/EventManager.cs
@@ -6,8 +6,14 @@ public static class EventManager
     public static event Action TimerStart;
     public static event Action TimerStop;
     public static event Action<float> TimerUpdate;
+    public static event Action<bool> TimerPause;
 
     public static void OnTimerStart() => TimerStart?.Invoke();
     public static void OnTimerStop() => TimerStop?.Invoke();
     public static void OnTimerUpdate(float value) => TimerUpdate?.Invoke(value);
+    public static void OnTimerPause(bool isPaused) => TimerPause?.Invoke(isPaused);
+
+    public static event Action GameWin;
+
+    public static void OnGameWin() => GameWin?.Invoke();
 }
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int totalPoints;
+
+    void Awake()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        Target.TargetHit += AddPoints;
     }
+
+    void OnDisable()
+    {
+        Target.TargetHit -= AddPoints;
+    }
+
+    void GameOver()
+    {
+        //Everything that happens when the player runs out of time
+        //Is Sent to the "You Failed screen" and can retry
+    }
+
+    public void AddPoints(int points) => totalPoints += points;
 }
