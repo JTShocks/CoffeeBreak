121166f8501fd0efea8f485e7e2281336e621683
JTShocks <jacobdreyer2002@gmail.com>
2025-01-17 20:11:42 -0500

    Update PlayerController.cs
    Added logic for hitting something on the screen, to be used with enemies

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    //how far the player can shoot
+    [Tooltip("How far the player can smack things from")]
+    public float rayLength;
+    public LayerMask canBeHit;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, rayLength, canBeHit))
+            {
+
+            }
     }
 }
