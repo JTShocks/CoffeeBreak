0edf3282b7d95846bf5ee5a614d0bcd3ff4f2cc0
JTShocks <jacobdreyer2002@gmail.com>
2025-01-19 14:40:35 -0500

    Final build

--- Assets/_Scripts/Shortcut.cs
+++ Assets/_Scripts/Shortcut.cs
@@ -17,7 +17,11 @@ public class Shortcut : Target
 
         //Send an event to switch the camera spline
         EventManager.OnSwitchCart(shortCutCart, cartStartPoint);
-        animator?.SetTrigger("OpenShortcut");
+        if(animator != null)
+        {
+            animator.SetTrigger("OpenShortcut");
+        }
+
                 base.OnHit();
     }
 }
