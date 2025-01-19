d8c835d7888ae46f45133b65ed03b73217c1551a
JTShocks <jacobdreyer2002@gmail.com>
2025-01-18 21:31:27 -0500

    Added models

--- Assets/_Scripts/CameraGuy.cs
+++ Assets/_Scripts/CameraGuy.cs
@@ -6,5 +6,10 @@ public class CameraGuy : MonoBehaviour
 {
     [SerializeField] List<Sprite> hurtSprites;
 
-    
+    //Camera guy functions
+
+    //Shows up at a random point of the screen
+    //Waits a few seconds to take your picture
+        //If he succeeds, flashes your screen
+            //lose points and slow down
 }
